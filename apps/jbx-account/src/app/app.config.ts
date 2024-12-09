38ad79c00f8f6dc5282ef62c220e2b43f728fd97
Jonathan GALLAIS <jgallais@groupeisagri.com>
2024-12-09 16:50:02 +0100

    fix: add i18n hydratation

--- apps/jbx-account/src/app/app.config.ts
+++ apps/jbx-account/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideClientHydration, withEventReplay, withIncrementalHydration } from '@angular/platform-browser';
+import { provideClientHydration, withEventReplay, withI18nSupport, withIncrementalHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideClientHydration(withEventReplay(), withIncrementalHydration()),
+    provideClientHydration(withEventReplay(), withIncrementalHydration(), withI18nSupport()),
     //provideZoneChangeDetection({ eventCoalescing: true }),
     provideExperimentalZonelessChangeDetection(),
     provideRouter(appRoutes,withComponentInputBinding(), withViewTransitions()),
